8c7ba00d6b62c840e9c49d59d1f6847f081b0afb
s152144 <cornelius.a.r@gmail.com>
2017-01-05 14:33:15 +0100

    implementation of matmult_blk, but it may not be correct

--- Question1.c
+++ Question1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 
+int MIN(int a, int b) {
+  if(a < b) { return a; }
+  return b;
+}
 //QUESTION2
 //OP1
 void matmult_mnk(int m,int n,int k,double **A,double **B,double **C){
@@ -121,30 +125,60 @@ void matmult_nat(int m,int n,int k,double **A,double **B,double **C){
   matmult_mnk(m,n,k,A,B,C);
 }
 
-void matmult_blk(int m,int n,int k,double **A,double **B,double **C){
-  //printf("matmult_blk: m=%i, n=%i, k=%i\n", m, n, k);
-  int i, j, l;
-  const int blockSize = 4;
+void matmult_blk(int m,int n,int k, double **A,double **B,double **C, int bs){
+  //printf("matmult_blk: m=%i, n=%i, k=%i\n, bs=%i", m, n, k, bs);
+  //order is actually int1, int3, int2
+  int i, j, l, i2, j2, l2;
+  int ie = 0;
+  int je = 0;
+  int ke = 0;
+
+  if(m % bs != 0) { ie = 1; }
+  if(n % bs != 0) { je = 1; }
+  if(k % bs != 0) { ke = 1; }
+  
   for (i = 0; i < m; i++) {
     for (j = 0; j < n; j++) {
       C[i][j]=0;
     }
   }
 
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < n; j++) {
-      for (l = 0; l < k - (k%blockSize) ; l+= blockSize) {
-	C[i][j] = C[i][j] + A[i][l]*B[l][j];
-	C[i][j] = C[i][j] + A[i][l+1]*B[l+1][j];
-	C[i][j] = C[i][j] + A[i][l+2]*B[l+2][j];
-	C[i][j] = C[i][j] + A[i][l+3]*B[l+3][j];
-      }
-      
-      if(k % blockSize > 0) {
-        for (l = k - (k % blockSize); l < k; l++) {
-	  C[i][j] = C[i][j] + A[i][l]*B[l][j];
+  for (i = 0; i < m/bs + ie; i++) { 
+    for (j = 0; j < n/bs + je; j++) {   
+      for (l = 0; l < k/bs + ke; l++) {
+        for(i2 = i*bs; i2 < MIN((i+1)*bs, m); i2++) { 
+          for(j2 = j*bs; j2 < MIN((j+1)*bs, n); j2++) {     
+            for(l2 = l*bs; l2 < MIN((l+1)*bs, k); l2++) {
+	      C[i2][j2] = C[i2][j2] + A[i2][l2]*B[l2][j2];
+            }
+          }  
         }
       }
     }
   }
+  /*
+  i2 = 0; 
+  j2 = 0;
+  int b1, b2;
+  for (l = 0; l < (k/bs + ke); l++) {
+    for (l2 = 0; l2 < (k/bs + ke); l2++) {
+      for (b1 = 0; b1 < bs; b1++) {
+        for(b2 = 0; b2 < bs; b2++) {
+          C[i2][j2] = C[i2][j2] + A[i2][b2]*B[b2][j2]; 
+        }
+        i2++;
+        j2++;
+      }
+    
+
+  for (i = 0; i < m/bs + ie; i++) {
+    for(i2 = i*bs; i2 < MIN((i+1)*bs, m); i2++) {
+
+    for (j = 0; j < n/bs + je; j++) { 
+    for(l2 = l*bs; l2 < MIN((l+1)*bs, k); l2++) {
+         
+             
+          for(j2 = j*bs; j2 < MIN((j+1)*bs, n); j2++) {     
+  }
+  */
 }
